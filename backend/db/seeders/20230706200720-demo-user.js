26535b44dfc4faf9ca681c9cddbcb390443c8f81
Bo James Hancock <RobertJamesH@gmail.com>
2023-07-07 09:37:02 -0700

    Fixed seeder file to comply with new restraints

--- backend/db/seeders/20230706200720-demo-user.js
+++ backend/db/seeders/20230706200720-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'Bo',
+        lastName: 'James',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Devon',
+        lastName: 'Weldon',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Ben',
+        lastName: 'Cooper',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')

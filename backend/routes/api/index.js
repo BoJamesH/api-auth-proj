d9b3c1edfa9bf38742bc1b9a5e579d5ffc56498f
Bo James Hancock <RobertJamesH@gmail.com>
2023-07-10 13:17:30 -0700

    seeded and tested preliminary get all spots route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -22,6 +22,8 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
+router.use('/spots', spotsRouter)
+
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,3 +2,19 @@
 // Import the express package
 const express = require('express');
 const { Spot, User, SpotImage, Booking, Review } = require('../../db/models');
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    const allSpots = await Spot.findAll({
+        // include: {
+        //     // model: {
+        //     //     SpotImage,
+        //     //     Review
+        //     // }
+        // },
+    });
+    res.json(allSpots);
+})
+
+module.exports = router;

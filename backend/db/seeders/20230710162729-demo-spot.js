a084ca02074944230b7d2a62ae22855854947e35
Bo James Hancock <RobertJamesH@gmail.com>
2023-07-10 09:54:15 -0700

    added postman test collection and demo-spot seeder file

---
+++ backend/db/seeders/20230710162729-demo-spot.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const { Spot } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Spot.bulkCreate([
+      {
+        firstName: 'Bo',
+        lastName: 'James',
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        firstName: 'Devon',
+        lastName: 'Weldon',
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        firstName: 'Ben',
+        lastName: 'Cooper',
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], { validate: true });
+  },
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};

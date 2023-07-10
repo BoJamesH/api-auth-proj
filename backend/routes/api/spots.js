bc650a7693e97c8faf798e3e4ec06d68c299bea7
Bo James Hancock <RobertJamesH@gmail.com>
2023-07-10 16:30:25 -0700

    added current user spot route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,6 +5,17 @@ const { Spot, User, SpotImage, Booking, Review } = require('../../db/models');
 
 const router = express.Router()
 
+router.get('/current', async (req, res) => {
+      // Get the current user's ID from the authenticated user's data
+      const userId = req.user.id;
+      // Retrieve all spots owned by the current user using the userId
+      const spots = await Spot.findAll({
+        where: { ownerId: userId },
+      });
+      // Send the spots as the response
+      res.status(200).json({ Spots: spots });
+});
+
 router.get('/', async (req, res, next) => {
     const allSpots = await Spot.findAll({
         // include: {
@@ -14,7 +25,8 @@ router.get('/', async (req, res, next) => {
         //     // }
         // },
     });
-    res.json(allSpots);
+    res.json({
+      Spots: allSpots});
 })
 
 module.exports = router;

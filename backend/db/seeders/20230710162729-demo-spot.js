38ab18012daf18e746f47175d4c13e4854c4fab0
Bo James Hancock <RobertJamesH@gmail.com>
2023-07-10 13:30:09 -0700

    inserted booking seeders

--- backend/db/seeders/20230710162729-demo-spot.js
+++ backend/db/seeders/20230710162729-demo-spot.js
@@ -1,6 +1,6 @@
 'use strict';
 const {Spot} = require('../models')
-const bcrypt = require("bcryptjs");
+
 let options = {}
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
--- backend/db/seeders/20230710201610-demo-booking.js
+++ backend/db/seeders/20230710201610-demo-booking.js
@@ -1,25 +1,78 @@
 'use strict';
 
+let options = {}
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    await Booking.bulkCreate([
+      {
+        spotId: 1,
+        userId: 1,
+        startDate: new Date('2023-07-11'),
+        endDate: new Date('2023-07-15'),
+      },
+      {
+        spotId: 3,
+        userId: 2,
+        startDate: new Date('2023-07-12'),
+        endDate: new Date('2023-07-17'),
+      },
+      {
+        spotId: 5,
+        userId: 3,
+        startDate: new Date('2023-07-15'),
+        endDate: new Date('2023-07-19'),
+      },
+      {
+        spotId: 7,
+        userId: 4,
+        startDate: new Date('2023-07-18'),
+        endDate: new Date('2023-07-22'),
 
+      },
+      {
+        spotId: 10,
+        userId: 1,
+        startDate: new Date('2023-07-21'),
+        endDate: new Date('2023-07-25'),
+      },
+      {
+        spotId: 13,
+        userId: 2,
+        startDate: new Date('2023-07-25'),
+        endDate: new Date('2023-07-30'),
+      },
+      {
+        spotId: 15,
+        userId: 3,
+        startDate: new Date('2023-07-28'),
+        endDate: new Date('2023-08-02'),
+      },
+      {
+        spotId: 18,
+        userId: 4,
+        startDate: new Date('2023-08-02'),
+        endDate: new Date('2023-08-06'),
+      },
+      {
+        spotId: 20,
+        userId: 1,
+        startDate: new Date('2023-08-05'),
+        endDate: new Date('2023-08-09'),
+      },
+      {
+        spotId: 22,
+        userId: 2,
+        startDate: new Date('2023-08-08'),
+        endDate: new Date('2023-08-12'),
+      }
+    ], options)
+  },
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Bookings', null, options)
   }
 };

87889f983ec7a1ca8430fce1f3aae8e22eb2dd1b
Bo James Hancock <RobertJamesH@gmail.com>
2023-07-12 16:07:33 -0700

    Finished and locally tested Add Image to Review based on ReviewId

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -48,6 +48,35 @@ router.get('/current', requireAuth, async (req, res, next) => {
     res.json( {"Reviews": formattedReviews} )
 })
 
+router.post('/:reviewId/images', requireAuth, async (req, res, next) => {
+    const thisReviewId = req.params.reviewId;
+    const userId = req.user.id;
+    const { url } = req.body;
+    const review = await Review.findByPk(thisReviewId);
+
+    if (!review) return res.status(404).json({ message: 'Review could not be found' })
+    if (review.userId !== userId) {
+        const err = new Error();
+        err.status = 403;
+        err.message = 'You are forbidden from adding pictures to reviews that do not belong to you.'
+        throw err;
+    };
+
+    const imageCount = await ReviewImage.count({
+        where: {
+          reviewId: thisReviewId,
+        },
+    });
+    if (imageCount > 9) return res.status(403).json({ message: 'Maximum number of images for this resource was reached' })
+    const newReviewImage = await ReviewImage.create({
+        reviewId: thisReviewId,
+        url,
+    });
+
+    const { createdAt, updatedAt, reviewId, ...newReviewImageData } = newReviewImage.toJSON();
+    res.json(newReviewImageData);
+})
+
 // router.get('/')
 
 module.exports = router;

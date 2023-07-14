a2cd8d49c068a06be4493c61595a9c5980ab1ccc
Bo James Hancock <RobertJamesH@gmail.com>
2023-07-14 15:46:28 -0700

    Attempted fix of delete spot-images

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -9,8 +9,8 @@ router.delete('/:imageId', requireAuth, async (req, res, next) => {
     const imageId = req.params.imageId;
     const userId = req.user.id;
     const deleteSpotImage = await SpotImage.findByPk(imageId);
-    const spotInQuestion = await Spot.findByPk(deleteSpotImage.spotId)
     if (!deleteSpotImage) return res.status(404).json({message: "That image could not be found"})
+    const spotInQuestion = await Spot.findByPk(deleteSpotImage.spotId)
     if (spotInQuestion.ownerId !== userId) return res.status(403).json({message: "You are not allowed to delete images that do not belong to your properties"})
 
     await deleteSpotImage.destroy();

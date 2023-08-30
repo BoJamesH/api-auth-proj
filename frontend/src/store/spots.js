6e42563d7436b0f1f6e4a94d82cbc6f50dad04b0
Bo James Hancock <RobertJamesH@gmail.com>
2023-08-30 14:26:14 -0700

    One more

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -147,7 +147,7 @@ export const updateSpot = (spotId, spot, spotImages) => async (dispatch) => {
     });
     if (response.ok) {
       const newSpot = await response.json();
-      dispatch(postSpotImages(spotImages, spotId))
+      await dispatch(postSpotImages(spotImages, spotId))
       await dispatch(fetchSpot(spotId)) // DO I NEED THIS?
       // await dispatch(postSpotImages(spotImages, spotId))
       return newSpot
